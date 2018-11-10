276c1fefc17e9aed0182337a65153dd39b60d4d1
dhruvgupta167 <gdhruv167@gmail.com>
2018-11-10 17:41:47 +0530

    Sync Messaging

--- frontend/script.js
+++ frontend/script.js
@@ -8,12 +8,28 @@ $(function () {
     let msglist = $('#msglist')
     let sendbtn = $('#sendmsg')
     let msgbox = $('#msgbox')
+    let loginbtn = $('#login')
 
+    loginbtn.click(function () {
+        console.log('Button Clicked')
+        socket.username = $('#user').val()
+
+        $('#login-div').css("display", "none")
+        $('#chat-div').css("display", "block")
+    })
+    
     sendbtn.click(function () {
-        socket.emit('send_msg', {message: msgbox.val()})
+        socket.emit('send_msg', {
+            message: msgbox.val(),
+            user: socket.username
+        })
     })
 
     socket.on('recv_msg', function (data) {
-        msglist.append($('<li>' + data.message + '</li>'))
+        if (data.user === socket.username) {
+            msglist.append(`Me :   ${data.message} <br>`)
+        } else {
+            msglist.append(`${data.user}: ${data.message} <br>`)
+        }
     })
 })
